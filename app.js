11eb87eacd6d33089deb1ea75ee3ad65484c48e9
dang  dung <dungconmetheu@gmail.com>
2023-04-18 12:12:14 +0700

    up up

--- app.js
+++ app.js
@@ -7,5 +7,24 @@ Splide.defaults = {
     type: 'loop',
     arrows: boolean = false,
     autoplay: true,
-    interval: number = 2000
+    interval: number = 3000
 }
+
+// Banner Mobile
+document.addEventListener('DOMContentLoaded', function () {
+    new Splide('#image-banner').mount();
+});
+
+// Banner Mobile
+document.addEventListener('DOMContentLoaded', function () {
+    new Splide('#image-product__mb').mount();
+});
+
+// comment detail product
+const titles = document.querySelectorAll('.comment-title h4');
+titles.forEach((title) => {
+    title.onclick = function () {
+        document.querySelector('.comment-title h4.active').classList.remove('active')
+        this.classList.add('active')
+    }
+})
